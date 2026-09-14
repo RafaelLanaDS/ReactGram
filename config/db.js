27eda92ff90e56102ec5d3c62a54747a95f7b65c
Rafael Lana <134884057+RafaelLanaDS@users.noreply.github.com>
2026-09-14 19:56:36 -0300

    coneção com o mongo db realizada

--- config/db.js
+++ config/db.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose")
 
-//connection
+const dbUser = process.env.BD_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8taofxr.mongodb.net/?retryWrites=true&w=majority`
+        )
+
+        console.log("Conectou no Banco")
+
+        return dbConn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
+
+//connection ykpBQRB1dMhL11O3 wRm3EqEM323iNqg3
